86640bf38b93228ff491d25fd4d77a280ff88323
Cebe74 <cebe74@github.com>
2020-01-15 21:44:12 +0100

    Rewrote renumber algorithm, should work fine with complex goto statements

--- extension.js
+++ extension.js
@@ -9,41 +9,41 @@ const workbenchConfig = vscode.workspace.getConfiguration('amstrad-basic-helper'
 var renumIncrement = workbenchConfig.get('renumIncrement');
 
 function updateDiagnostics(document, collection) {
-	if (document && path.extname(document.fileName).toUpperCase() == '.BAS') {
-		if (!this.oCodeGeneratorJs) {
-			this.oCodeGeneratorJs = new CodeGeneratorJs({
-				lexer: new BasicLexer(),
-				parser: new BasicParser(),
-				tron: false
-			});
-		} else {
-			this.oCodeGeneratorJs.reset();
-		}
+    if (document && path.extname(document.fileName).toUpperCase() == '.BAS') {
+        if (!this.oCodeGeneratorJs) {
+            this.oCodeGeneratorJs = new CodeGeneratorJs({
+                lexer: new BasicLexer(),
+                parser: new BasicParser(),
+                tron: false
+            });
+        } else {
+            this.oCodeGeneratorJs.reset();
+        }
 
-		var text = document.getText();
+        var text = document.getText();
 
-		var outputVariables = {};
-		
-		var output = this.oCodeGeneratorJs.generate(text, outputVariables);
-		if (output.error) {
-			var outputError = output.error;
-			var iStartPos = outputError.pos
-			var iEndPos = iStartPos + ((outputError.value !== undefined) ? String(outputError.value).length : 0);
+        var outputVariables = {};
 
-			collection.set(document.uri, [{
-				code: '',
-				message: `${outputError.message}: '${outputError.value}'`,
-				range: new vscode.Range(
-					document.positionAt(iStartPos),
-					document.positionAt(iEndPos)
-				),
-				severity: vscode.DiagnosticSeverity.Error,
-				source: ''
-			}]);
-		} else {
-			collection.delete(document.uri);
-		}
-	}
+        var output = this.oCodeGeneratorJs.generate(text, outputVariables);
+        if (output.error) {
+            var outputError = output.error;
+            var iStartPos = outputError.pos
+            var iEndPos = iStartPos + ((outputError.value !== undefined) ? String(outputError.value).length : 0);
+
+            collection.set(document.uri, [{
+                code: '',
+                message: `${outputError.message}: '${outputError.value}'`,
+                range: new vscode.Range(
+                    document.positionAt(iStartPos),
+                    document.positionAt(iEndPos)
+                ),
+                severity: vscode.DiagnosticSeverity.Error,
+                source: ''
+            }]);
+        } else {
+            collection.delete(document.uri);
+        }
+    }
 }
 
 /**
@@ -51,79 +51,79 @@ function updateDiagnostics(document, collection) {
  */
 function activate(context) {
 
-	const collection = vscode.languages.createDiagnosticCollection('Amstrad Basic');
-	if (vscode.window.activeTextEditor) {
-		updateDiagnostics(vscode.window.activeTextEditor.document, collection);
-	}
+    const collection = vscode.languages.createDiagnosticCollection('Amstrad Basic');
+    if (vscode.window.activeTextEditor) {
+        updateDiagnostics(vscode.window.activeTextEditor.document, collection);
+    }
 
-	context.subscriptions.push(vscode.workspace.onDidChangeTextDocument(editor => {
-		if (editor) {
-			updateDiagnostics(editor.document, collection);
-		}
-	}));
+    context.subscriptions.push(vscode.workspace.onDidChangeTextDocument(editor => {
+        if (editor) {
+            updateDiagnostics(editor.document, collection);
+        }
+    }));
 
-	let renumberMe = vscode.commands.registerCommand('amstrad-basic-helper.renum', function () {
-		if (path.extname(vscode.window.activeTextEditor.document.fileName).toUpperCase() == '.BAS') {
-			let editor = vscode.window.activeTextEditor;
-			if (!editor) {
-				vscode.window.showInformationMessage('Please open an Amstrad Basic document first!');
-				return; // No open text editor
-			}
+    let renumberMe = vscode.commands.registerCommand('amstrad-basic-helper.renum', function () {
+        if (path.extname(vscode.window.activeTextEditor.document.fileName).toUpperCase() == '.BAS') {
+            let editor = vscode.window.activeTextEditor;
+            if (!editor) {
+                vscode.window.showInformationMessage('Please open an Amstrad Basic document first!');
+                return; // No open text editor
+            }
 
-			if (collection.has(editor.document.uri)) {
-				vscode.window.showErrorMessage('The current Amstrad Basic file has errors and cannot be renumbered!');
-				return;
-			}
+            if (collection.has(editor.document.uri)) {
+                vscode.window.showErrorMessage('The current Amstrad Basic file has errors and cannot be renumbered!');
+                return;
+            }
 
-			var linelist = [];
+            var linelist = [];
 
-			let edit = new vscode.WorkspaceEdit();
-			var ls;
+            let edit = new vscode.WorkspaceEdit();
+            var ls;
 
-			//build array of old and new line numbers
-			for (ls = 0; ls < editor.document.lineCount; ls++) {
-				let line = editor.document.lineAt(ls);
-				var lineNumber = 1;
-				try {
-					lineNumber = parseInt(line.text.match(/^\d+/)[0]);
-				}
-				catch (ex) {
+            //build array of old and new line numbers
+            for (ls = 0; ls < editor.document.lineCount; ls++) {
+                let line = editor.document.lineAt(ls);
+                var lineNumber = 1;
+                try {
+                    lineNumber = parseInt(line.text.match(/^\d+/)[0]);
+                }
+                catch (ex) {
 
-				}
+                }
 
-				let lineString = line.text.substring(lineNumber.toString().length + 1);
-				var l = new renum.BasicLine(lineNumber, lineString);
-				linelist.push(l);
-			}
+                let lineString = line.text.substring(lineNumber.toString().length + 1);
+                var l = new renum.BasicLine(lineNumber, lineString);
+                linelist.push(l);
+            }
 
-			var newLineNumber = renumIncrement;
-			linelist.forEach(line => {
-				line.newLineNumber = newLineNumber;
-				newLineNumber += renumIncrement;
-			});
+            var newLineNumber = renumIncrement;
+            linelist.forEach(line => {
+                line.newLineNumber = newLineNumber;
+                newLineNumber += renumIncrement;
+            });
 
-			renum.Renumber(linelist);
+            renum.Renumber(linelist);
 
-			for (ls = 0; ls < linelist.length; ls++) {
-				let line = editor.document.lineAt(ls);
-				//if (line.text.trimRight().length > 0) {
-				//console.log("LINE: " + linelist[ls].newLineString);
-				edit.replace(editor.document.uri, line.range, linelist[ls].newLineString);
-				//}
-			}
+            for (ls = 0; ls < linelist.length; ls++) {
+                let line = editor.document.lineAt(ls);
+                //if (line.text.trimRight().length > 0) {
+                //console.log("LINE: " + linelist[ls].newLineString);
+                edit.replace(editor.document.uri, line.range, linelist[ls].newLineString);
+                //}
+            }
 
-			vscode.workspace.applyEdit(edit);
-			vscode.window.showInformationMessage('Renum complete!');
-		}
-	});
+            vscode.workspace.applyEdit(edit);
+            vscode.window.showInformationMessage('Renum complete!');
+        }
+    });
 
-	context.subscriptions.push(renumberMe);
+    context.subscriptions.push(renumberMe);
 }
 exports.activate = activate;
 
 function deactivate() { }
 
 module.exports = {
-	activate,
-	deactivate
+    activate,
+    deactivate
 }
--- renum/renum.js
+++ renum/renum.js
@@ -2,8 +2,8 @@ class BasicLine {
     constructor(lineNumber, lineString) {
         this._lineNumber = lineNumber;
         this._lineString = lineString;
-    }    
-    
+    }
+
     get lineNumber() { return this._lineNumber; }
 
     get lineString() { return this._lineString; }
@@ -15,53 +15,6 @@ class BasicLine {
     get newLineString() { return this.newLineNumber + ' ' + this.lineString; }
 }
 
-function Renumber(lines) {
-    lines.forEach(line => {
-        // https://regex101.com/r/Izi7x3/8
-        // another algo for ON xx GOTO ON xx GOSUB (should run first)
-        var match = /on\ +[^:]+\ +(?:goto|gosub)(?:\ +([\d,\ ]+))/gi;
-        let gonum = line.lineString.match(match);
-        if (gonum != null && gonum.length > 0) {
-            for (var x = 0; x < gonum.length; x++) {
-                var matches = [...gonum[x].matchAll(/\d+/g)];
-                for (const m of matches.reverse()) {
-                    let lineNumberToReplace = m[0];
-                    let lineNumberIndex = m.index;
-                    let lineNumberLength = lineNumberToReplace.length;
-                    let newLineNumber = GetNewLineNum(lines, lineNumberToReplace);
-                    if (newLineNumber === -1) {
-                        // This shouldn't not happen anymore as we do not call renumber if the document is invalid
-                        continue;
-                    }                    
-                    line.lineString = line.lineString.substring(0, lineNumberIndex) + newLineNumber + line.lineString.substring(lineNumberIndex + lineNumberLength);
-                }
-            }
-        }
-        else {
-            // https://regex101.com/r/Sb7Vgf/1
-            var match = /(?:goto|gosub|restore|then|else)\ +\d+/gi;
-            let gonum = line.lineString.match(match);
-            if (gonum != null && gonum.length > 0) {
-                for (var x = 0; x < gonum.length; x++) {
-                    var m = gonum[x].match(/\d+/);
-                    for (var y = 0; y < m.length; y++) {
-                        let lineNumberToReplace = m[y];
-                        let newLineNumber = GetNewLineNum(lines, lineNumberToReplace);
-                        if (newLineNumber === -1) {
-                            // This shouldn't not happen anymore as we do not call renumber if the document is invalid
-                            continue;
-                        }
-                        let newgonum = gonum[x].replace(lineNumberToReplace, newLineNumber);
-                        line.lineString = line.lineString.replace(gonum[x], newgonum);
-                    }
-
-                }
-            }
-        }
-    });
-    return lines;
-}
-
 function GetNewLineNum(lines, originalLineNumber) {
     var newLineNumber = -1;
     lines.forEach(line => {
@@ -70,6 +23,30 @@ function GetNewLineNum(lines, originalLineNumber) {
         }
     });
     return newLineNumber;
+}
+
+function Renumber(lines) {
+    lines.forEach(line => {
+        // in theory we should differentiate simple jump instructions from ON xx GOTO or ON xx GOSUB instructions
+        // but we rely on the fact that we cannot renumber an invalid document by design
+        var matches = [...line.lineString.matchAll(/(?:goto|gosub|restore|then|else)(?:\ +[\d,\ ]+)/gi)];
+        for (const match of matches.reverse()) {
+            var offset = match.index;            
+            var lineNumbers = [...match[0].matchAll(/\d+/g)];
+            for (const ln of lineNumbers.reverse()) {
+                let lineNumberToReplace = ln[0];
+                let lineNumberIndex = ln.index + offset;
+                let lineNumberLength = lineNumberToReplace.length;
+                let newLineNumber = GetNewLineNum(lines, lineNumberToReplace);
+                if (newLineNumber === -1) {
+                    // This shouldn't not happen anymore as we do not call renumber if the document is invalid
+                    continue;
+                }
+                line.lineString = line.lineString.substring(0, lineNumberIndex) + newLineNumber + line.lineString.substring(lineNumberIndex + lineNumberLength);
+            }
+        }
+    });
+    return lines;
 }
 
 exports.BasicLine = BasicLine;
